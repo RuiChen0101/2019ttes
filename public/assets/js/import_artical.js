c1abf72fc24a4dd500d1967b76ec489ad79853de
SpiritCat <gamain0101@gmail.com>
2019-02-18 23:00:46 +0800

    back to origin

--- public/assets/js/import_artical.js
+++
@@ -1,7 +0,0 @@
-importArtical("common","common_rule_artical");
-function importArtical(dist,from){
-  var temp = document.getElementById(from);
-  var clon = temp.content.cloneNode(true);
-  document.getElementById(dist).appendChild(clon);
-  console.log(from+" add");
-}
