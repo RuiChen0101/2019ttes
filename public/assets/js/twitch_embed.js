1fd2f7ab60a8482864c4f36520e99b590f880383
SpiritCat <gamain0101@gmail.com>
2019-02-20 15:44:08 +0800

    twitch section respones to channel online

--- public/assets/js/twitch_embed.js
+++ public/assets/js/twitch_embed.js
@@ -1,9 +1,10 @@
 jQuery(function($){
   $.get('https://api.twitch.tv/kraken/streams/?client_id=l0vnzza1qmgq679uyaok9hk9lh81yu&channel=taipeitech_esports_club', function(channel) {
-    if (channel["streams"] != null) {
-      console.log("onlive");
+    console.log(channel["streams"]);
+    if (channel["streams"].length != 0) {
+      $("#mu-live").css("display","inline");
     } else {
-      console.log("nothing");
+      $("#mu-live").css("display","none");
     }
   });
 });
