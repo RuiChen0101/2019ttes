14600bb8f167c59b67500f03d51343785b896ae8
SpiritCat <gamain0101@gmail.com>
2019-02-27 12:07:06 +0800

    slider img fix, tw embed mathod change

--- public/assets/js/twitch_embed.js
+++ public/assets/js/twitch_embed.js
@@ -2,14 +2,14 @@ jQuery(function($){
   $.get('https://api.twitch.tv/kraken/streams/?client_id=l0vnzza1qmgq679uyaok9hk9lh81yu&channel=taipeitech_esports_club', function(channel) {
     if (channel["streams"].length != 0) {
       $("#mu-live").css("display","inline");
+      new Twitch.Embed("twitch-embed", {
+        width: '100%',
+        height: '100%',
+        layout: "video",
+        channel: "taipeitech_esports_club"
+      });
     } else {
       $("#mu-live").css("display","none");
     }
   });
-});
-new Twitch.Embed("twitch-embed", {
-  width: '100%',
-  height: '100%',
-  layout: "video",
-  channel: "taipeitech_esports_club"
 });
